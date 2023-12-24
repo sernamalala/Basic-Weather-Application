1cb1d314625cf445622ab193a773b8bd9de5d2e0
sernamalala <sernaprograms@gmail.com>
2023-12-24 08:46:57 +0200

    Weather data and time is now api accurate

--- src/index.js
+++ src/index.js
@@ -6,9 +6,33 @@ function displayWeather(response){
 
     let currentCity = document.querySelector("#current-city");
     currentCity.innerHTML = response.data.city;
+
+    let humidity = document.querySelector(".humidity");
+    humidity.innerHTML = response.data.temperature.humidity+"%";
+
+    let wind = document.querySelector(".wind-value");
+    wind.innerHTML = response.data.wind.speed;
+
+    let condition = document.querySelector(".weather-condition");
+    condition.innerHTML = response.data.condition.description;
+
+    let dateText = document.querySelector(".date");
+    let date = new Date (response.data.time *1000);
+    dateText.innerHTML = formatDate(date);
 }
 
+function formatDate(date){
 
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days[date.getDay()];
+
+    if(minutes<10){
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hour}:${minutes}`
+}
 
 function searchCity(city){
 
