484a6ae303b585f7d3010b2c2b1faf6d709c0266
sernamalala <sernaprograms@gmail.com>
2023-12-24 08:07:38 +0200

    integrated api into project with basic search functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+function displayWeather(response){
+    console.log(response);
+    let weatherValue = document.querySelector(".weather-value");
+    let apiWeatherValue = Math.round(response.data.temperature.current);
+    weatherValue.innerHTML = apiWeatherValue;
+
+    let currentCity = document.querySelector("#current-city");
+    currentCity.innerHTML = response.data.city;
+}
+
+
+
+function searchCity(city){
+
+    let apiKey = "67afdaf57o30ta1c59fb3ae425f8488b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayWeather);
+
+}
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
 
@@ -6,7 +28,9 @@ function handleSearchSubmit(event){
 
     let currentCity = document.querySelector("#current-city");
     currentCity.innerHTML = input.value;
+    searchCity(input.value);
 }
 
 let searchForm= document.querySelector("#search-form");
-searchForm.addEventListener("submit",handleSearchSubmit)
+searchForm.addEventListener("submit",handleSearchSubmit);
+searchCity("Cape Town");
